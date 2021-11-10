5857642aeaec7ad28fadcb0629235095a8137df5
oldchevy <nick.olszowy@gmail.com>
2021-11-09 22:09:44 -0500

    first update for rmpk

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ class App extends React.Component {
               <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935z"/>
             </svg>
           </a>
-          <h3 className="nav nav-pills py-2 hide-nonmobile">Daily Fantasy Sports CPA</h3>
+          <h3 className="nav nav-pills py-2 hide-nonmobile">RMPK Accounting</h3>
           <ul className="nav nav-pills hide-mobile">
             {this.state.items.map((item, i) => {
               return (
@@ -117,7 +117,7 @@ class App extends React.Component {
         <div className="court">
           <div className="over-court position-relative p-3 p-md-5">
             <div className="col-sm-5 p-lg-5 mx-auto my-5">
-              <h1 className="display-4 fw-normal hide-mobile">Daily Fantasy Sports CPA</h1>
+              <h1 className="display-4 fw-normal hide-mobile">RMPK Accounting</h1>
               <p className="lead fw-normal">Welcome to my website. I specialize in advising YOU about daily fantasy sports tax implications. Read on to find out about what I offer!</p>
             </div>
           </div>

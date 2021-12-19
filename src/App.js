7041b61b0b8d61f02bd9e6afb147946efdccee26
oldchevy <nick.olszowy@gmail.com>
2021-12-19 18:00:25 -0500

    initial logo update for rmpk

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import {ReactComponent as WhiteMainLogo} from './static_assets/RMPK-Logo/RMPK-White-Main.svg';
 
 class App extends React.Component {
 
@@ -65,7 +66,7 @@ class App extends React.Component {
           }
           return item;
         } else {
-          return;
+          return null;
         }
       });
     this.setState({ items: items});
@@ -78,10 +79,8 @@ class App extends React.Component {
    
       <header className="footer-header sticky-top py-1">
         <nav id="navbar" className="container d-flex flex-row justify-content-between bottom-buffer">
-          <a className="py-2 navbar-brand" href="#" aria-label="Home">
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-trophy-fill" viewBox="0 0 16 16">
-              <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935z"/>
-            </svg>
+          <a className="py-2 navbar-brand logo-medium" href="#" aria-label="Home">
+            <WhiteMainLogo />
           </a>
           <h3 className="nav nav-pills py-2 hide-nonmobile">RMPK Accounting</h3>
           <ul className="nav nav-pills hide-mobile">
@@ -213,13 +212,11 @@ class App extends React.Component {
       <footer className="footer-header">
         <div className="container py-3">
           <div className="row">
-            <div className="col">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-trophy hide-mobile" viewBox="0 0 16 16">
-                <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935zM3.504 1c.007.517.026 1.006.056 1.469.13 2.028.457 3.546.87 4.667C5.294 9.48 6.484 10 7 10a.5.5 0 0 1 .5.5v2.61a1 1 0 0 1-.757.97l-1.426.356a.5.5 0 0 0-.179.085L4.5 15h7l-.638-.479a.501.501 0 0 0-.18-.085l-1.425-.356a1 1 0 0 1-.757-.97V10.5A.5.5 0 0 1 9 10c.516 0 1.706-.52 2.57-2.864.413-1.12.74-2.64.87-4.667.03-.463.049-.952.056-1.469H3.504z"/>
-              </svg>
-              <small className="d-block mo-margin text-muted">© RMPK Accounting, 2022</small>
+            <div className="col fit-logo">
+              <WhiteMainLogo className="logo-footer"/>
             </div>
             <div className="col right-side">
+              <small className="d-block mo-margin text-muted">© RMPK Accounting, 2022</small>
               {/* <a
                 className="hide-mobile"
                 href="https://twitter.com/dailyfantasycpa?lang=en"
